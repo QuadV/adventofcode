9ac8204f612df64f484ddd73bf7ded790626e9e8
QuadV <vionaquadros@gmail.com>
2022-10-24 23:07:58 +0200

    day 3 a

--- src/daily_code/day_3.py
+++ src/daily_code/day_3.py
@@ -1,9 +1,39 @@
 from src.utils.helper_functions import read_file
+import numpy as np
 
 
 def day_3_a(path):
     """
-    Calculate
+    Calculate gamma_rate and epsilon_rate
+    Args:
+        path: Data path
+
+    Returns:
+
+    """
+    data = read_file(path=path)
+    buckets = np.zeros(len(data[0])-1, int)
+    for value in data:
+        value_array = np.array(list(map(int, list(value)[:-1])))
+        buckets += value_array
+
+    mid_value = len(data) / 2
+    gamma_rate = ''
+    epsilon_rate = ''
+    for value in buckets:
+        if value > mid_value:
+            gamma_rate += '1'
+            epsilon_rate += '0'
+        else:
+            gamma_rate += '0'
+            epsilon_rate += '1'
+    binary_mul = int(gamma_rate, 2) * int(epsilon_rate, 2)
+    return binary_mul
+
+
+def day_3_b(path):
+    """
+    Calculate gamma_rate and epsilon_rate
     Args:
         path: Data path
 
@@ -12,9 +42,3 @@ def day_3_a(path):
     """
     data = read_file(path=path)
 
-    for value in data:
-        print(value)
-        print(bin(int(value, base=2)))
-        break
-
-    # binary_mul = lambda a,b : bin(int(a, 2) * int(b, 2))
