447defa9d6d225bffa8121dda8289d707e252b81
QuadV <vionaquadros@gmail.com>
2022-10-29 13:08:17 +0200

    day 3

--- src/daily_code/day_3.py
+++ src/daily_code/day_3.py
@@ -31,9 +31,33 @@ def day_3_a(path):
     return binary_mul
 
 
+def get_values(data, rating_value):
+    """
+    Reduce array according to the bit criteria for different rating value
+    Args:
+        data:
+        rating_value:
+
+    Returns:
+
+    """
+    values = np.array(list(map(lambda x: list(map(int, list(x[:-1]))), data)))
+    for num in range(len(values[0])):
+        sum_pos = np.sum(values, axis=0)[num]
+        if sum_pos >= len(values) / 2:
+            # bit 1 is more in position num
+            remove_element = rating_value[0]
+        else:
+            remove_element = rating_value[1]
+
+        values = values[values[:, num] == remove_element]
+        if len(values) == 1:
+            return values[0].dot(2 ** np.arange(values[0].size)[::-1])
+
+
 def day_3_b(path):
     """
-    Calculate gamma_rate and epsilon_rate
+    Calculate oxygen_generator_rating * CO2_scrubber_rating
     Args:
         path: Data path
 
@@ -41,4 +65,11 @@ def day_3_b(path):
 
     """
     data = read_file(path=path)
+    oxygen_generator_rating = get_values(data=data, rating_value=[1, 0])
+    CO2_scrubber_rating = get_values(data=data, rating_value=[0, 1])
+
+    return oxygen_generator_rating * CO2_scrubber_rating
+
+
+
 
